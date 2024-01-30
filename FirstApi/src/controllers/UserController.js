d9ec2c2f54db854908fe906466f32f8db43d39d2
luiz <afsilvaluiz@gmail.com>
2024-01-30 15:46:08 -0300

    feat: receiving parameters from the URL

--- FirstApi/src/controllers/UserController.js
+++ FirstApi/src/controllers/UserController.js
@@ -2,11 +2,11 @@ const users = require('../mocks/users')
 
 module.exports = {
   listUsers(request, response) {
-    const { order } = request.query
+    const  order = request.query
 
     const sortedUsers = users.sort((a, b) => {
       if (order === 'desc') {
-        return a.id < b.id ? 1: -1
+        return a.id < b.id ? 1 : -1
       }
 
       return a.id > b.id ? 1 : -1
@@ -14,5 +14,21 @@ module.exports = {
 
     response.writeHead(200, { 'Content-Type': 'application/json' })
     response.end(JSON.stringify(sortedUsers))
+  },
+
+  getUserById(request, response) {
+    const { id } = request.params
+
+    const user = users.find((user) => user.id === Number(id))
+
+    if (!user) {
+      response.writeHead(400, { 'Content-Type': 'application/json' })
+      response.end(JSON.stringify({ error: 'User not found'}))
+    }else {
+      response.writeHead(200, { 'Content-Type': 'application/json' })
+      response.end(JSON.stringify(user))
+
+    }
+    
   }
 }
