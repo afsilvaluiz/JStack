908f124c83c4e7b8694174577ebdc816f34af611
luiz <afsilvaluiz@gmail.com>
2024-02-16 18:16:58 -0300

    feat: create ContactsRepository.

---
+++ myContacts-api/src/app/repositories/ContactsRepository.js
@@ -0,0 +1,20 @@
+const { v4: uuidv4 } = require('uuid');
+
+const contacts = [
+  {
+    id: uuidv4(),
+    name: 'Mateus',
+    phone: '123123213',
+    category_id: uuidv4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
