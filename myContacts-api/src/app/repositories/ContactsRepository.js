99e4d021c4f59114b3086ad280d8899e626b4b79
luiz <afsilvaluiz@gmail.com>
2024-02-26 15:53:15 -0300

    feat: add find by id function to list users

--- myContacts-api/src/app/repositories/ContactsRepository.js
+++ myContacts-api/src/app/repositories/ContactsRepository.js
@@ -8,12 +8,27 @@ const contacts = [
     phone: '123123213',
     category_id: v4(),
   },
+  {
+    id: v4(),
+    name: 'Jose',
+    email: 'jose@mail.com',
+    phone: '432123213',
+    category_id: v4(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
     });
   }
 }
