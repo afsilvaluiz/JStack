7f9126300374fcc4ffbe87395c53f48eff946682
luiz <afsilvaluiz@gmail.com>
2024-03-04 17:07:43 -0300

    chore: studying spread operator

---
+++ modernJS/05-spreadOperator.js
@@ -0,0 +1,17 @@
+const user = {
+  firstName: 'Luiz',
+  lastName: 'Silva',
+  age: 41,
+  instagram: 'afsilvaluiz',
+  skills: ['Back-end', 'Front-end', '3d-modelling'],
+  full_name: 'Luiz Silva',
+};
+
+// SPREAD OPERATOR
+// PRINCIPLE OF IMMUTABILITY, NEVER CHANGE OBJECT PROPERTIES DIRECTLY
+
+const updatedUser = {
+  // CHANGES MADE HERE, AFTER ...USER, WILL OVERWRITE THE PROPERTIES
+  // ARRAYS WORK THE SAME WAY
+  ...user
+};
