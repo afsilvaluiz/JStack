482d1c4a43165024c9ee37cc5ae4edb2dd880b79
luiz <afsilvaluiz@gmail.com>
2024-03-04 17:53:39 -0300

    chore: studying templateStrings

--- modernJS/06-templateString.js
+++ modernJS/06-templateString.js
@@ -5,3 +5,7 @@
 // `` -> ALLOWS LINE BREAKS
 //       CONSIDERS EVERYTHING INSIDE A STRING (SPACES AND TABS)
 //       ALLOWS CONCATENATING JS EXPRESSIONS WITH ${EXPRESSION HERE}
+
+const name = 'Luiz';
+
+console.log(`Welcome ${name}`);
