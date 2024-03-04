7ac7fa399960548ecb62ffb034e2d8c19f9eac0a
luiz <afsilvaluiz@gmail.com>
2024-03-04 17:41:12 -0300

    chore: studying spread operator

--- modernJS/05-spreadOperator.js
+++ modernJS/05-spreadOperator.js
@@ -5,6 +5,7 @@ const user = {
   instagram: 'afsilvaluiz',
   skills: ['Back-end', 'Front-end', '3d-modelling'],
   full_name: 'Luiz Silva',
+  active: false,
 };
 
 // SPREAD OPERATOR
@@ -13,5 +14,10 @@ const user = {
 const updatedUser = {
   // CHANGES MADE HERE, AFTER ...USER, WILL OVERWRITE THE PROPERTIES
   // ARRAYS WORK THE SAME WAY
-  ...user
+  ...user,
+  skills: [...user.skills, 'Marketing'],
+  active: true,
 };
+
+console.log({user});
+console.log({updatedUser})
