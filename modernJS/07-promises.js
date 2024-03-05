cc17df67d8db8474c545bcdffc1afacf90f175ed
luiz <afsilvaluiz@gmail.com>
2024-03-05 16:46:11 -0300

    chore: studying promises

--- modernJS/07-promises.js
+++ modernJS/07-promises.js
@@ -1,20 +1,24 @@
 const apiCall = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve('Success!');
+    // reject('Error!');
   }, 2000);
 });
 
-apiCall.then((value) => {
-  console.log(value); // Logs: Success!
-}).catch((error) => {
-  console.error(error); // Logs any error that occurred
-});
+// apiCall.then((value) => {
+//   console.log(value); // Logs: Success!
+// }).catch((error) => {
+//   console.error(error); // Logs any error that occurred
+// });
 
 async function run() {
   try {
-    const response = await apiCall();
+    const response = await apiCall;
     console.log(response); // Logs: Success!
+    console.log('After the promise'); // This line will be executed immediately after the Promise is created
   } catch (error) {
     console.error(error); // Logs any error that occurred
   }
 }
+
+run();
