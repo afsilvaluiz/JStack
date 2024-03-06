aa41df149aa9d10f18e877096c0da8c3b9028d15
luiz <afsilvaluiz@gmail.com>
2024-03-06 16:39:05 -0300

    chore: studying helpers

--- modernJS/08-helpers.js
+++ modernJS/08-helpers.js
@@ -1,19 +1,19 @@
 const tech = 'Node.js';
 const array = ['React', 'Node.js', 'Ruby'];
 
-const includesString = tech.includes('ode.js'); // MATCHES PART OF THE STRING
+const includesString = tech.includes('Node'); // MATCHES PART OF THE STRING
 // CASE SENSITIVE
 // BOOLEAN
 
 const includesArray = array.includes('React'); // MATCHES A POSITION IN THE ARRAY
 // IF THERE'S A POSITION IN THE ARRAY WITH A STRING === TO THE ONE PASSED RETURNS A BOOLEAN
 // CASE SENSITIVE
-console.log({includesArray});
 
-const startsWith = tech.startsWith('Node'); // MATCHES THE CHARACTER SEQUENCE
+const startsWith = tech.startsWith('no'); // MATCHES THE CHARACTER SEQUENCE
 // AT THE BEGINNING OF A STRING, OR A SINGLE CHARACTER
 // CASE SENSITIVE
 // BOOLEAN
+console.log({ startsWith });
 
 const endsWith = tech.endsWith('Node'); // MATCHES THE CHARACTER SEQUENCE
 // AT THE END OF A STRING, OR A SINGLE CHARACTER
