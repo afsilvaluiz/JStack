942f1e406b100188ceedfb68f6e0777bfbde6e71
luiz <afsilvaluiz@gmail.com>
2024-03-13 16:14:39 -0300

    chore: study of expressions in JSX

--- reactFundamentals/src/App.js
+++ reactFundamentals/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const category = 'Posts of the week';
+
 function App() {
   return (
     <>
-      <h1>Component App</h1>
-      <h2>Subtitle</h2>
+      <h1>Haura's blog</h1>
+      <h2>{category}</h2>
     </>
   )
 }
