d8f51574d5df2a46ac3e6a144ca92d89b9352290
luiz <afsilvaluiz@gmail.com>
2024-03-13 10:56:35 -0300

    chore: configure webpack

--- reactFundamentals/webpack.config.js
+++ reactFundamentals/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 };
