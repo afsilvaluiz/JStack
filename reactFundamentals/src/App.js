9c7b8f965db78e27ff9d12d0ccaf7416a59f6e73
luiz <afsilvaluiz@gmail.com>
2024-03-14 16:30:51 -0300

    chore: study props

--- reactFundamentals/src/App.js
+++ reactFundamentals/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-const category = 'Posts of the week';
-
 function App() {
   return (
     <>
       <h1>Haura's blog</h1>
-      <h2>{category}</h2>
+      <h2>Posts of the week</h2>
     </>
   )
 }
