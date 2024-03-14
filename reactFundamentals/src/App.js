4f145e41121b0fb54aef7aac04d8e4ccb301a74c
luiz <afsilvaluiz@gmail.com>
2024-03-14 16:52:57 -0300

    chore: study props

--- reactFundamentals/src/App.js
+++ reactFundamentals/src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 
+import Post from "./Post";
+
 function App() {
   return (
     <>
       <h1>Haura's blog</h1>
       <h2>Posts of the week</h2>
+
+      <hr />
+
+      <Post 
+        title='News headline 01' 
+        subtitle='News subheadline 01' 
+      />
+      <Post 
+        title='News headline 02' 
+        subtitle='News subheadline 02' 
+      />
+      <Post 
+        title='News headline 03' 
+        subtitle='News subheadline 03' 
+      />
+
     </>
   )
 }
