3f68cde27a49731ff7aeac2e1fa3adf780a7559a
luiz <afsilvaluiz@gmail.com>
2024-03-18 08:31:40 -0300

    chore: study of prop types

--- reactFundamentals/src/App.js
+++ reactFundamentals/src/App.js
@@ -6,25 +6,28 @@ import Header from "./Header";
 function App() {
   return (
     <>
-      <Header title="Haura's blog">
+      <Header>
         <h2>Posts of the week</h2>
       </Header>
 
       <hr />
 
       <Post
+        likes={20}
         post={{
           title:'News headline 01',
           subtitle: 'News subheadline 01',
         }}
       />
       <Post
+        likes={10}
         post={{
           title:'News headline 02',
           subtitle: 'News subheadline 02',
         }}
       />
       <Post
+        likes={50}
         post={{
           title:'News headline 03',
           subtitle: 'News subheadline 03',
