4611960a66e1d75ed4d769971c9f1a94eed8dd51
luiz <afsilvaluiz@gmail.com>
2024-03-20 16:34:32 -0300

    style: create the styled component

--- reactFundamentals/src/components/Header/index.js
+++ reactFundamentals/src/components/Header/index.js
@@ -1,17 +1,21 @@
-import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import Button from '../Button';
 
-import styles from './Header.scss';
-
 import { ThemeContext } from '../../context/ThemeContext';
+
+const Title = styled.h1`
+  color: #21838E;
+`;
 
 export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return (
     <>
-      <h1 className={styles.title}>{props.title}</h1>
+      <Title>{props.title}</Title>
       <Button onClick={onToggleTheme}>
         Change theme
       </Button>
