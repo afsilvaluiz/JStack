17884bded94003d73f0b6c6f2ca587c02aed0482
luiz <afsilvaluiz@gmail.com>
2024-03-20 15:33:56 -0300

    chore: organizing file structure

---
+++ reactFundamentals/src/components/App/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import { ThemeProvider } from '../../context/ThemeContext';
+import Header from "../Header";
+import Post from "../Post";
+
+import styles from './App.scss';
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true, removed: false },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false, removed: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true, removed: false },
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: 50,
+      }
+    ]);
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) => prevState.map(
+      post => (
+        post.id === postId
+          ? { ...post, removed: true }
+          : post
+      )
+    ));
+  }
+
+  return (
+    <ThemeProvider>
+      <Header>
+        <h2 className={styles.title}>
+          Posts of the week
+          <button onClick={handleRefresh}>Update</button>
+        </h2>
+      </Header>
+
+      <hr />
+
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          likes={post.likes}
+          onRemove={handleRemovePost}
+          post={post}
+        />
+      ))}
+    </ThemeProvider>
+  )
+}
+
+export default App;
---
+++ reactFundamentals/src/components/Header/index.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import Button from '../Button';
+
+import styles from './Header.scss';
+
+import { ThemeContext } from '../../context/ThemeContext';
+
+export default function Header(props) {
+  const { onToggleTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <h1 className={styles.title}>{props.title}</h1>
+      <Button onClick={onToggleTheme}>
+        Change theme
+      </Button>
+      {props.children}
+    </>
+  )
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Header.defaultProps = {
+  title: "Haura's blog",
+};
