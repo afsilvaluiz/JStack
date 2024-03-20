5978e835a9241b2bc9b216a1a7cf5c1cc16ff68c
luiz <afsilvaluiz@gmail.com>
2024-03-20 15:59:21 -0300

    chore: configure styled-components

---
+++ reactFundamentals/src/styles/global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  body {
+    background: #042331;
+    color: #fff;
+    font-family: sans-serif;
+  }
+`;
