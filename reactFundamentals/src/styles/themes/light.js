a2c61715fafce56f8e8b0ae168993aaa75358161
luiz <afsilvaluiz@gmail.com>
2024-03-21 15:08:35 -0300

    style: configure themes

--- reactFundamentals/src/styles/themes/light.js
+++ reactFundamentals/src/styles/themes/light.js
@@ -2,6 +2,10 @@ import commonStyles from './common'
 
 export default {
   ...commonStyles,
-  backgroundColor: '#fff',
-  textColor: '#222',
+  backgroundColor: '#7EC2C9',
+  textColor: '#7EC2C9',
+  headerBackgroundColor: '#042331',
+  footerBackgroundColor: '#042331',
+  postBackgroundColor: '#074D5E',
+  smallColor: '#ccc',
 };
