bdc68b28d4654c4f42a4b5655dcf97c8c92b0b2d
luiz <afsilvaluiz@gmail.com>
2024-03-21 15:53:44 -0300

    feat: create toggle between dark and light

--- reactFundamentals/src/components/Footer/index.js
+++ reactFundamentals/src/components/Footer/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({ onToggleTheme, selectedTheme }) {
   return (
     <Container>
       <span>Haura's blog. All rights reserved</span>
-      <button type="button">🌞</button>
+      <button 
+        type="button" 
+        onClick={onToggleTheme} 
+      >
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+      </button>
     </Container>
   );
 }
