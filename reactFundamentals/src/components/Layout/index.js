2743ad5780eb2d704e4aa04a89725b8239931060
luiz <afsilvaluiz@gmail.com>
2024-03-21 12:41:09 -0300

    refactor: Layout create

---
+++ reactFundamentals/src/components/Layout/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../Header';
+import PostLists from '../PostList';
+import Footer from '../Footer';
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <PostLists />
+      <Footer />
+    </>
+  );
+}
+
+
+
