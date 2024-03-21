2645783f9af1b6c120adffa93eea0d41fcdea889
luiz <afsilvaluiz@gmail.com>
2024-03-21 15:54:11 -0300

    feat: create toggle between dark and light

--- reactFundamentals/src/components/Header/index.js
+++ reactFundamentals/src/components/Header/index.js
@@ -2,11 +2,16 @@ import React, { useContext } from 'react';
 
 import { Container } from './styles';
 
-export default function Header() {
+export default function Header({ onToggleTheme, selectedTheme }) {
   return (
     <Container>
       <h1>Haura's Blog</h1>
-      <button type="button">🌞</button>
+      <button 
+        type="button" 
+        onClick={onToggleTheme} 
+      >
+        { selectedTheme === 'dark' ? '🌞' : '🌚'}
+      </button>
     </Container>
   );
 }
