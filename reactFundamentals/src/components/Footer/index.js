731a921355bcdaa147e8c8d3cd401a095a6c473e
luiz <afsilvaluiz@gmail.com>
2024-03-21 12:39:26 -0300

    refactor: create footer

---
+++ reactFundamentals/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Footer() {
+  return (
+    <Container>
+      <span>Haura's blog. All rights reserved</span>
+      <button type="button">🌞</button>
+    </Container>
+  );
+}
---
+++ reactFundamentals/src/components/Footer/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  background: #21838E;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 24px;
+  border-radius: 10px;
+  justify-content: space-between;
+  margin-top: 24px;
+
+  span {
+    color: #042331;
+  }
+
+  button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+  }
+`;
