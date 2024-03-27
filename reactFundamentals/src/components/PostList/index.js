66ee8302a54c2282a05ac93e503be845c0c5f117
luiz <afsilvaluiz@gmail.com>
2024-03-27 12:02:40 -0300

    chore: study of class component structure

--- reactFundamentals/src/components/PostList/index.js
+++ reactFundamentals/src/components/PostList/index.js
@@ -5,19 +5,37 @@ import Post from './Post';
 
 import posts from './posts';
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+export default class PostsList extends React.Component {
+  render () {
+      return (
+        <Container>
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              title={post.title}
+              description={post.description}
+            />
+          ))}
+        </Container>
+      );
+  }
 }
+
+// export default function PostsList() {
+//   return (
+//     <Container>
+//       {posts.map((post) => (
+//         <Post
+//           key={post.id}
+//           title={post.title}
+//           description={post.description}
+//         />
+//       ))}
+//     </Container>
+//   );
+// }
+
+
 
 
 
