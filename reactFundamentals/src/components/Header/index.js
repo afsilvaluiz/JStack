ba9e6d2cdf7a28e0524726f66ae9309c2548b856
luiz <afsilvaluiz@gmail.com>
2024-03-27 12:40:28 -0300

    chore: study of class component structure

--- reactFundamentals/src/components/Header/index.js
+++ reactFundamentals/src/components/Header/index.js
@@ -1,17 +1,35 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 
 import { Container } from './styles';
 
-export default function Header({ onToggleTheme, selectedTheme }) {
-  return (
-    <Container>
-      <h1>Haura's Blog</h1>
-      <button 
-        type="button" 
-        onClick={onToggleTheme} 
-      >
-        {selectedTheme === 'dark' ? '🌞' : '🌚'}
-      </button>
-    </Container>
-  );
+export default class Header extends Component {
+  render() {
+      const { onToggleTheme, selectedTheme } = this.props;      
+
+      return (
+        <Container>
+          <h1>Haura's Blog</h1>
+          <button 
+            type="button" 
+            onClick={onToggleTheme} 
+          >
+            {selectedTheme === 'dark' ? '🌞' : '🌚'}
+          </button>
+        </Container>
+      );
+  }
 }
+
+// export default function Header({ onToggleTheme, selectedTheme }) {
+//   return (
+//     <Container>
+//       <h1>Haura's Blog</h1>
+//       <button 
+//         type="button" 
+//         onClick={onToggleTheme} 
+//       >
+//         {selectedTheme === 'dark' ? '🌞' : '🌚'}
+//       </button>
+//     </Container>
+//   );
+// }
