c3c750a75a41ebfcba7bb69ec58e6fc6f8e277ee
luiz <afsilvaluiz@gmail.com>
2024-04-03 17:21:50 -0300

    chore: study of component componentWillUnMount

--- reactFundamentals/src/components/Layout/index.js
+++ reactFundamentals/src/components/Layout/index.js
@@ -4,14 +4,29 @@ import Footer from '../Footer';
 import Header from '../Header';
 import PostLists from '../PostList';
 
-export default function Layout() {
-  return (
-    <>
-      <Header />
-      <PostLists />
-      <Footer />
-    </>
-  );
+export default class Layout extends React.Component {
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    console.log('Component will unmount');
+    document.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    console.log('scrolled...')
+  }
+
+  render () {
+    return (
+      <>
+        <Header />
+        <PostLists />
+        <Footer />
+      </>
+    );
+  }
 }
 
 
