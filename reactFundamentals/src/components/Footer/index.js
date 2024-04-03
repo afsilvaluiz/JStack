bc2a3db4598562ec243cd308590b9f61ea38d74f
luiz <afsilvaluiz@gmail.com>
2024-04-03 16:28:45 -0300

    chore: study of consuming Context API

--- reactFundamentals/src/components/Footer/index.js
+++ reactFundamentals/src/components/Footer/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 import { Container } from './styles';
 
-export default function Footer({ onToggleTheme, selectedTheme }) {
+export default function Footer() {
+  const { theme, handleToggleTheme } = useContext(ThemeContext);
+
   return (
     <Container>
       <span>Haura's blog. All rights reserved</span>
       <button 
         type="button" 
-        onClick={onToggleTheme} 
+        onClick={handleToggleTheme} 
       >
-        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+        {theme === 'dark' ? '🌞' : '🌚'}
       </button>
     </Container>
   );
